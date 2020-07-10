299fc9775956bb9cb224383edae259741608f2b8
wongxy <xiyao.wong@foxmail.com>
2020-07-10 17:10:56 +0800

    Update docs; Add 'Action queue' example

---
+++ sample/plugins/bot_test_queue.py
@@ -0,0 +1,10 @@
+from iotbot import Action, GroupMsg
+from iotbot.decorators import equal_content, not_botself
+
+action = Action(123456, queue=False, queue_delay=2)  # 唯一写法
+
+
+@not_botself
+@equal_content('queue')
+def receive_group_msg(ctx: GroupMsg):
+    action.send_group_text_msg(ctx.FromGroupId, '这条消息每次发送间隔不会低于2秒。。。')
