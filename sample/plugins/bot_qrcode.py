4800e1b01cce0aad12936ae987ad2f0d50a96e8d
wongxy <xiyao.wong@foxmail.com>
2020-07-17 09:39:21 +0800

    "生成二维码"插件

---
+++ sample/plugins/bot_qrcode.py
@@ -0,0 +1,35 @@
+'''
+生成二维码+{数据(文字)}
+'''
+import base64
+import io
+
+from iotbot import FriendMsg
+from iotbot import GroupMsg
+from iotbot.decorators import in_content
+from iotbot.decorators import not_botself
+from iotbot.sugar import Picture
+
+try:
+    import qrcode
+except ImportError:
+    raise ImportError('请先安装依赖库: pip install qrcode, pillow')
+
+
+def gen_qrcode(text: str) -> str:
+    img = qrcode.make(text)
+    img_buffer = io.BytesIO()
+    img.save(img_buffer)
+    return base64.b64encode(img_buffer.getvalue()).decode()
+
+
+@not_botself
+@in_content('生成二维码')
+def receive_group_msg(ctx: GroupMsg):
+    Picture(pic_base64=gen_qrcode(ctx.Content.replace('生成二维码', '')))
+
+
+@not_botself
+@in_content('生成二维码')
+def receive_friend_msg(ctx: FriendMsg):
+    Picture(pic_base64=gen_qrcode(ctx.Content.replace('生成二维码', '')))
